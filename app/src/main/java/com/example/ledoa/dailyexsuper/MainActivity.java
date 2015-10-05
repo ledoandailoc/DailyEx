d6aeebc7ed77e117462ac9c621a471c5e62686bd
AnhtuanUit <anhtuan7194@gmail.com>
2015-10-06 01:32:40 +0700

    v

--- app/src/main/java/com/example/ledoa/dailyexsuper/MainActivity.java
+++ app/src/main/java/com/example/ledoa/dailyexsuper/MainActivity.java
@@ -33,8 +33,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    int mIconMenuLeft[] = {R.drawable.icon_timban, R.drawable.icon_thongbao, R.drawable.icon_tab,
-            R.drawable.icon_thongbao, R.drawable.icon_logout};
+    int mIconMenuLeft[] = {R.drawable.icon_thongbao, R.drawable.icon_tab_timban_nau, R.drawable.icon_danhba,
+            R.drawable.icon_caidat, R.drawable.icon_logout};
     String mTextMenuLeft[] = {"Thông báo mới","Thêm bạn", "Danh bạ","Cài đặt riêng tư","Thoát"};
 
     TextView mTvTabLuyenTap, mTvTabLichTap, mTvTabBaiTap, mTvTabMangXaHoi, mTvActionBarTitle, mTvUsernameMenuLeft;
